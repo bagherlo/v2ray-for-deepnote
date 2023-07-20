4e81bd651746a9e30e382ebc675e525a066487b3
bagherlo <bagherlo@live.com>
2023-07-21 01:45:58 +0330

    Update deep-vless.sh

--- deep-vless.sh
+++ deep-vless.sh
@@ -32,19 +32,14 @@ echo ""
 yellow "使用前请注意："
 red "1. 我已知悉本项目有可能触发 Deepnote 封号机制"
 red "2. 我不保证脚本其搭建节点的稳定性"
-read -rp "是否安装脚本？ [Y/N]：" yesno
 
-if [[ $yesno =~ "Y"|"y" ]]; then
     rm -f web config.json
     yellow "开始安装..."
     wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
     unzip temp.zip
     rm -f temp.zip
     mv v2ray web
-    read -rp "请设置UUID（如无设置则使用脚本默认的）：" uuid
-    if [[ -z $uuid ]]; then
-        uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
-    fi
+    uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
     rm -f config.json
     cat << EOF > config.json
 {
@@ -100,7 +95,3 @@ EOF
     yellow "别忘记给项目点一个免费的Star！"
     echo ""
     yellow "更多项目，请关注：小御坂的破站"
-else
-    red "已取消安装，脚本退出！"
-    exit 1
-fi
